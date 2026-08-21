62925adea1bd77fb848d6c0f375b3c9e73a37a3f
Amrit Kahlon <amrit@scalar.com>
2026-08-21 14:41:37 -0700

    Fix versioning conflict in _version.py

--- src/scalar_sdk/_version.py
+++ src/scalar_sdk/_version.py
@@ -1,9 +1,4 @@
 # File generated from our OpenAPI spec by Scalar. See README.md for details.
 
-<<<<<<< HEAD
-__title__ = "scalarApi"
-__version__ = "0.4.0"  # x-release-please-version
-=======
 __title__ = "scalar"
 __version__ = "0.1.0"  # x-release-please-version
->>>>>>> d787c6833be3a8f11983bd7137f00d8c0d36a16e
